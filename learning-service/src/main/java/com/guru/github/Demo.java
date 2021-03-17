ebde5284bfaad4b6a000241dcfcdf1b343ff90ef
854144163@qq.com <xuhao@dianxiaomi.com>
2021-03-17 20:51:24 +0800

    [change]Design Pattern

---
+++ learning-service/src/main/java/com/guru/github/Demo.java
@@ -0,0 +1,8 @@
+package com.guru.github;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, It is my first project.");
+    }
+}
---
+++ learning-service/src/main/java/com/guru/github/design_pattern/DesignPatternDemo.java
@@ -0,0 +1,8 @@
+package com.guru.github.design_pattern;
+
+public class DesignPatternDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Design Pattern Demo Learning");
+    }
+}
