f50d41f7bca890e8017e2fcd0d3bd24f2873f7e3
854144163@qq.com <xuhao@dianxiaomi.com>
2021-04-15 10:18:05 +0800

    [change]algorithm

--- learning-service/src/main/java/com/guru/github/algorithm/Question050.java
+++ learning-service/src/main/java/com/guru/github/algorithm/Question050.java
@@ -8,9 +8,9 @@ import java.util.Scanner;
 public class Question050 {
 
     //题目：有五个学生，每个学生有3门课的成绩，从键盘输入以上数据（包括学生号，姓名，三门课成绩），计算出平均成绩，把原有的数据和计算出的平均分数存放在磁盘文件 "stud "中
-    //程序分析：
+    //程序分析：输入 运算 流处理
 
-    public static void main(String[] args) { //个人练习：
+    public static void main(String[] args) { //个人练习：没进行对应练习，主要考虑到没什么思维练习
 
         Scanner ss = new Scanner(System.in);
         String [][] a = new String[5][6];
